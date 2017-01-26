16b25e6bf5b9405bedd5fc104494feb228668ccb
Brendan Berg <lacrossefan25@gmail.com>
2017-01-26 16:48:41 -0700

    Update parrallax.js

--- parrallax.js
+++ parrallax.js
@@ -20,3 +20,21 @@ $(document).ready(function(){
 	});
 });
 
+
+
+var modal = document.getElementById('myModal');
+
+
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+
+
+
+}
